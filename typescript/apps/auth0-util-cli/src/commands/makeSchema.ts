534bbbc5d9b54bd2db1aa8d65487ff62fa24e3da
Holly Plank <hollyplankdev@gmail.com>
2024-07-05 13:13:26 -0500

    Old in progress commit

--- typescript/apps/auth0-util-cli/src/commands/makeSchema.ts
+++ typescript/apps/auth0-util-cli/src/commands/makeSchema.ts
@@ -1,6 +1,8 @@
 import { Command } from "@commander-js/extra-typings";
+import { Auth0ApiModel } from "@motd-ts/auth0-architecture";
 
 const program = new Command().argument("<output-file>").parse();
 const [outputFile] = program.args;
 
 console.log(outputFile);
+console.log(Auth0ApiModel);
---
+++ typescript/packages/auth0-architecture/src/index.ts
@@ -0,0 +1,3 @@
+export { default as Auth0ApiModel } from "./Auth0ApiModel";
+export { default as Auth0ApplicationModel } from "./Auth0ApplicationModel";
+export { default as Auth0TenantModel } from "./Auth0TenantModel";
